c4d36518282461f93b9bfe162beedbcc68f9aa0b
Ralf Zerres <ralf.zerres@networkx.de>
2020-09-24 09:06:19 +0200

    localization: update documentation strings
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- crates/localization/src/localization.rs
+++ crates/localization/src/localization.rs
@@ -1,11 +1,11 @@
 ///
 pub trait Localization {
-    /// Gets the current language by language key e.g. `en_US` or `de_DE`.
+    /// Gets the current language identified by a language key (e.g. `en_US`, `de_DE`).
     fn language(&self) -> &String;
 
-    /// Sets the current language by key e.g. `en_US` or `de_DE`.
+    /// Sets the current language identified by its language key (e.g. `en_US`, `de_DE`).
     fn set_language(&mut self, key: &str);
 
-    /// Gets the translated text for the given key. If there is no given translation the `key` will be returned as result.
+    /// Gets the translation string matched by the active language key. The function will retrun properties default string, if a translation string is missing.
     fn text(&self, key: String) -> String;
 }
